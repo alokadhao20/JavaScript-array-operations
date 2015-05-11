62184d1e7f9ce5d0be508834e545b05b95c50095
a.adho <a.adho@partner.samsung.com>
2015-05-11 13:22:14 +0530

    added method concat()

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
+var cars = ["Saab", "Volvo", "BMW"];
 
+// METHODS
 
+//1.concat() Joins two or more arrays, and returns a copy of the joined arrays
 
+var hege = ["Cecilie", "Lone"];
+var stale = ["Emil", "Tobias", "Linus"];
+var array3 = ["chrome", "firefox", "IE"];
+var children = hege.concat(stale,array3);
+console.log(children);
+// O/p [ 'Cecilie','Lone','Emil','Tobias','Linus','chrome','firefox','IE' ]
+  
+  
+  
+  
+  
+  
+  
